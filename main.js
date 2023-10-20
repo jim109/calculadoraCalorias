c9e5afb109b51a6a1e25d73df731d2529d7fc301
Jimmy Osma <josma109@gmail.com>
2023-10-19 23:45:37 -0500

    [UPDATE] validaciones y limpieza de formularios

--- main.js
+++ main.js
@@ -3,11 +3,20 @@ const resultado = document.getElementById('resultado');
 
 formularioCalculadora.addEventListener('submit', (e) => {
     e.preventDefault();
-    calcularCalorias();
 
+    //desestructuramos validarFormulario
+    const { exito, errores } = validarFormulario();
+
+    //validamos si la validacion da exito o error para ejecutar el resultado
+    if (exito) {
+        mostrarResultado();
+    }else {
+        mostrarMensajeDeError(errores)
+    }
 });
 
-function calcularCalorias() {
+//leemos los datos que traemos del formulario HTML
+const obtenerDatosFormulario = () => {
     const nombre = document.querySelector('#nombre').value;
     const tipoDocumento = document.querySelector('#tipoDocumento').value;
     const documento = document.querySelector('#numeroDocumento').value;
@@ -17,65 +26,114 @@ function calcularCalorias() {
     const actividad = document.querySelector('#actividad').value;
     const generoSeleccionado = document.querySelector('input[name="genero"]:checked').value;
 
-    const poblacion = edad < 60 ? edad < 30? "Joven" : "Adulto" : "Adulto Mayor"
-    console.log(poblacion);
+    const poblacion = edad < 60 ? edad < 30 ? "Joven" : "Adulto" : "Adulto Mayor"
 
-    
-
-    if (!edad || !peso || !altura || !actividad || !nombre || !documento) {
-        mostrarMensajeDeError("Por favor, rellena todos los campos");
-        console.log("Por favor, rellena todos los campos");
-       mostrarMensajeDeError();
-    } else {
-        if ( generoSeleccionado === 'M') {
-            const calorias = Math.floor(actividad *  (10 * peso) + (6.25 * altura) - (5 * edad) + 5);
-            aparecerResultado()
-            resultado.innerHTML = `            
-            <div class="card-body d-flex flex-column justify-content-center align-items-center w-100 h-100 my-3 shadow text-center">
-                <h2>El paciente ${nombre}</h2>
-                <p>Identificado con ${tipoDocumento} No. ${documento}</p>
-                <span>Requiere un total de <strong>${calorias}</strong> kcal para el sostenimiento de su TBM</span>
-                <span>El grupo poblacional al que perteneces es: <strong>${poblacion}</strong></span>
-            </div>`
-        
-        }else if (generoSeleccionado === 'F') {
-            const calorias = Math.floor(actividad *   (10 * peso) + (6.25 * altura) - (5 * edad) - 161);
-            aparecerResultado()
-            resultado.innerHTML = `
-            <div class="card-body d-flex flex-column justify-content-center align-items-center w-100 h-100 my-3 shadow text-center">
-                <h2>El paciente ${nombre}</h2>
-                <p>Identificado con ${tipoDocumento} No. ${documento}</p>
-                <span>Requiere un total de <strong>${calorias}</strong> kcal para el sostenimiento de su TBM</span>
-                <span>El grupo poblacional al que perteneces es: <strong>${poblacion}</strong></span>
-            </div>`
-        }
+    return {
+        nombre,
+        tipoDocumento,
+        documento,
+        edad,
+        peso,
+        altura,
+        actividad,
+        generoSeleccionado,
+        poblacion
     }
 
+}
+
+//Hacemos las validaciones necesarias para el formulario
+const validarFormulario =() => {
+    const { nombre, tipoDocumento, documento, edad, peso, altura, actividad, generoSeleccionado, poblacion } = obtenerDatosFormulario();
+    const errores = {};
+
+    if (!nombre || !tipoDocumento || !documento || !edad || !peso || !altura || !actividad || !generoSeleccionado || !poblacion) {
+        errores.formulario = 'Por favor, rellena todos los campos';
+    }
+    if (edad < 14 || edad > 120) {
+        errores.edad = 'Por favor, ingresa una edad válida';
+    }
+    if (peso < 20 || peso > 700) {
+        errores.peso ='Por favor, ingresa un peso válido';   
+    }
+    if (altura < 50 || altura > 250) {
+        errores.altura = 'Por favor, ingresa una altura válida';   
+    }
+    //validamos si tenemos algun error dentro del objeto, caso contrario exito = true
+    const exito = Object.keys(errores).length === 0;
+
+    return {
+        exito,
+        errores,
+    };
+}
+
+//hacemos el calculo de las calorias dependiendo de si es mujer o hombre
+const calcularCalorias = () => {
+    const { edad, peso, altura, actividad, generoSeleccionado } = obtenerDatosFormulario();
+
+    parcialCalorias = Math.floor(actividad *  (10 * peso) + (6.25 * altura) - (5 * edad))
+
+    const calorias = generoSeleccionado === 'M' 
+            ? (parcialCalorias + 5) : (parcialCalorias - 161);
+
+    return {
+        calorias,
+        ...obtenerDatosFormulario(),
+    }
 
 }
 
-function validarFormulario() {
-    const edad = document.querySelector('#edad').value;
-    const peso = document.querySelector('#peso').value;
-    const altura = document.querySelector('#altura').value;
-    const actividad = document.querySelector('#actividad').value;
+//imprimimos el resultado en el html
+const mostrarResultado = () => {
+    const { nombre, tipoDocumento, documento, poblacion, calorias } = calcularCalorias();
 
-    return edad && peso && altura && actividad;
-
+    aparecerResultado()
+    resultado.innerHTML = `
+    <div class="card-body d-flex flex-column justify-content-center align-items-center w-100 h-100 my-3 shadow text-center">
+        <h2>El paciente ${nombre}</h2>
+        <p>Identificado con ${tipoDocumento} No. ${documento}</p>
+        <span>Requiere un total de <strong>${calorias}</strong> kcal para el sostenimiento de su TBM.</span>
+        <span>El grupo poblacional al que pertenece es: <strong>${poblacion}</strong></span>
+    </div>`
+    //damos un tiempo para la limpieza del formulario con setTimeout
+    setTimeout(() => {
+        limpiarFormulario()
+    }, 8000);
 }
+
  //Formula hombres: valor actividad x (10 x peso en kg) + (6,25 × altura en cm) - (5 × edad en años) + 5
  //Formula mujeres: valor actividad x (10 x peso en kg) + (6,25 × altura en cm) - (5 × edad en años) - 161
 
+const limpiarFormulario = () => {
+    formularioCalculadora.reset();
+}
 
-function mostrarMensajeDeError(msg) {
+
+function mostrarMensajeDeError() {
+    //desestructuramos y sacamos los errores que obtuvimos
+    const { errores } = validarFormulario();
+
     const calculo = document.querySelector('#calculo');
     if (calculo) {
         calculo.remove();
     }
 
     const divError = document.createElement('div');
-    divError.className = 'd-flex justify-content-center align-items-center h-100';
-    divError.innerHTML = `<span class="alert alert-danger text-center">${msg}</span>`;
+    divError.className = 'd-flex flex-column justify-content-center align-items-between w-100  h-100';
+
+    if (errores.formulario) {
+        divError.innerHTML += `<span class="alert alert-danger text-center">${errores.formulario}</span>`;
+    }
+    if (errores.edad) {
+        divError.innerHTML += `<span class="alert alert-danger text-center">${errores.edad}</span>`;
+    }
+    if (errores.peso) {
+        divError.innerHTML += `<span class="alert alert-danger text-center">${errores.peso}</span>`;
+    }
+    if (errores.altura) {
+        divError.innerHTML += `<span class="alert alert-danger text-center">${errores.altura}</span>`;
+    }
 
     resultado.style.display = 'block';
     resultado.appendChild(divError);
@@ -83,7 +141,7 @@ function mostrarMensajeDeError(msg) {
     setTimeout(() => {
         divError.remove(); 
         desvanecerResultado();
-    }, 100000);
+    }, 10000);
 }
 
 
